5db35f4c519ed72d910cf15f4b9aed27812cb47a
Unknown <anbudokaiclavero@gmail.com>
2018-11-28 23:54:25 -0200

    Projeto Completo
    Commit All

---
+++ LaMaia/LaMaia/www/scripts/index.js
@@ -0,0 +1,13 @@
+﻿//  document.getElementById("btnOK").addEventListener("click", Logar());
+ 
+//  function Logar() {
+//     var usr = document.getElementById("edtTexto").value;
+//     // var senha = document.getElementById("edtSenha").value;
+//     console.log(usr);
+    
+//     if (usr == '') {
+//         // if (senha === '12345') {
+//         alert('Bem Vindo');
+//         // }
+//     }
+// } 
